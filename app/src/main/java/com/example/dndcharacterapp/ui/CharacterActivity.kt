99c24bb999a02b1f75813cb79356c4832f6c6a67
Jironiah <seandertal@gmail.com>
2024-05-08 18:32:39 +0200

    Mostrar doble dropdownmenu

--- app/src/main/java/com/example/dndcharacterapp/ui/CharacterActivity.kt
+++ app/src/main/java/com/example/dndcharacterapp/ui/CharacterActivity.kt
@@ -11,14 +11,11 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
-import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
@@ -35,13 +32,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import com.example.dndcharacterapp.models.alignment.Alignment as Alignments
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.dndcharacterapp.api.CrudApi
+import com.example.dndcharacterapp.models.character.HitPoints
 import com.example.dndcharacterapp.models.race.Race
-import com.example.dndcharacterapp.ui.ui.theme.DNDCharacterAppTheme
+import com.example.dndcharacterapp.ui.theme.DNDCharacterAppTheme
 
 class CharacterActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -68,11 +64,11 @@ class CharacterActivity : ComponentActivity() {
 @Composable
 fun MostrarEditText(
     racesList: List<Race>,
-    alignmentsList: List<Alignments>,
+    alignmentsList: List<com.example.dndcharacterapp.models.alignment.Alignment>?,
 ) {
     var expanded by remember { mutableStateOf(false) }
     var racesOptions by remember { mutableStateOf(racesList[0].name) }
-    var alignmentsOptions by remember { mutableStateOf(alignmentsList[0].name) }
+    var alignmentsOptions by remember { mutableStateOf(alignmentsList!![0].name) }
 
     Column(
         modifier = Modifier
@@ -88,19 +84,7 @@ fun MostrarEditText(
             horizontalArrangement = Arrangement.SpaceBetween,
             verticalAlignment = Alignment.CenterVertically
         ) {
-            TextField(
-                value = "",
-                onValueChange = { /*TODO*/ },
-                label = { Text("Name") },
-                modifier = Modifier.weight(1f)
-            )
-            Spacer(modifier = Modifier.width(16.dp))
-            TextField(
-                value = "",
-                onValueChange = { /*TODO*/ },
-                label = { Text("Level") },
-                modifier = Modifier.weight(1f)
-            )
+            Mostrar2TextField("Name", "Level")
         }
         //Inspiration Background
         Row(
@@ -112,12 +96,7 @@ fun MostrarEditText(
             val inspiration = remember { mutableStateOf(true) }
             Checkbox(checked = inspiration.value, onCheckedChange = {inspiration.value = it} )
             Spacer(modifier = Modifier.width(16.dp))
-            TextField(
-                value = "",
-                onValueChange = { /*TODO*/ },
-                label = { Text("Background") },
-                modifier = Modifier.weight(1f)
-            )
+            Mostrar1TextField("Background")
         }
         //Race Alignment
         Row(
@@ -125,60 +104,121 @@ fun MostrarEditText(
             horizontalArrangement = Arrangement.SpaceBetween,
             verticalAlignment = Alignment.CenterVertically
         ) {
-            var racesName : MutableList<String> = mutableListOf()
-            racesList.forEach {
+            val racesName: MutableList<String> = mutableListOf()
+            racesList!!.forEach {
                 racesName.add(it.name)
             }
-            MostrarDropDowns(list = racesName)
-            Spacer(modifier = Modifier.width(16.dp))
-            var alignmentsName : MutableList<String> = mutableListOf()
-            alignmentsList.forEach {
+            val alignmentsName: MutableList<String> = mutableListOf()
+            alignmentsList!!.forEach {
                 alignmentsName.add(it.name)
             }
-            MostrarDropDowns(list = alignmentsName)
 
+            MostrarDropDowns(list1 = racesName, list2 = alignmentsName)
         }
+        //HitPoint HitDie
+        Text(text = "HitPoints")
+
+
+            Mostrar1TextField(textoMostrar = "Maximum")
+            Mostrar1TextField(textoMostrar = "Current")
+            Mostrar1TextField(textoMostrar = "Temporary")
+
+
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
+//Esto mostrará un textfield para que se escriba el string
 @Composable
-fun MostrarDropDowns(list: List<String>): String {
-    val context = LocalContext.current
-    var expanded by remember { mutableStateOf(false) }
-    var selectedText by remember { mutableStateOf(list[0]) }
-
+fun Mostrar1TextField(textoMostrar: String) {
     Row(
         modifier = Modifier.fillMaxWidth(),
-        horizontalArrangement = Arrangement.SpaceBetween
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Box(modifier = Modifier.weight(1f)) {
+            TextField(
+                value = "",
+                onValueChange = { /*TODO*/ },
+                label = { Text(textoMostrar) }
+            )
+        }
+
+        Spacer(modifier = Modifier.width(8.dp))
+    }
+}
+
+@Composable
+fun Mostrar2TextField(textoMostrar1: String, textoMostrar2: String) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
     ) {
         Box(
             modifier = Modifier.weight(1f)
         ) {
+            TextField(
+                value = "",
+                onValueChange = { /*TODO*/ },
+                label = { Text(textoMostrar1) }
+            )
+        }
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Box(
+            modifier = Modifier.weight(1f)
+        ) {
+            TextField(
+                value = "",
+                onValueChange = { /*TODO*/ },
+                label = { Text(textoMostrar2) }
+            )
+        }
+    }
+}
+
+
+//Esto mostrará dos exposeddropdownmenusbox
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MostrarDropDowns(list1: List<String>, list2: List<String>) {
+    val context = LocalContext.current
+    var expanded1 by remember { mutableStateOf(false) }
+    var selectedText1 by remember { mutableStateOf(list1[0]) }
+    var expanded2 by remember { mutableStateOf(false) }
+    var selectedText2 by remember { mutableStateOf(list2[0]) }
+
+    Row(Modifier.fillMaxWidth()) {
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .padding(horizontal = 4.dp)
+        ) {
             ExposedDropdownMenuBox(
-                expanded = expanded,
+                expanded = expanded1,
                 onExpandedChange = {
-                    expanded = !expanded
+                    expanded1 = it
                 }
             ) {
                 TextField(
-                    value = selectedText,
+                    value = selectedText1,
                     onValueChange = {},
                     readOnly = true,
-                    trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
+                    trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded1) },
                     modifier = Modifier.menuAnchor()
                 )
 
                 ExposedDropdownMenu(
-                    expanded = expanded,
-                    onDismissRequest = { expanded = false }
+                    expanded = expanded1,
+                    onDismissRequest = { expanded1 = false }
                 ) {
-                    list.forEach { item ->
+                    list1.forEach { item ->
                         DropdownMenuItem(
                             text = { Text(text = item) },
                             onClick = {
-                                selectedText = item
-                                expanded = false
+                                selectedText1 = item
+                                expanded1 = false
                                 Toast.makeText(context, item, Toast.LENGTH_SHORT).show()
                             }
                         )
@@ -186,22 +226,46 @@ fun MostrarDropDowns(list: List<String>): String {
                 }
             }
         }
-        Spacer(modifier = Modifier.width(8.dp)) // Ajusta el tamaño del spacer según sea necesario
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .padding(horizontal = 4.dp)
+        ) {
+            ExposedDropdownMenuBox(
+                expanded = expanded2,
+                onExpandedChange = {
+                    expanded2 = it
+                }
+            ) {
+                TextField(
+                    value = selectedText2,
+                    onValueChange = {},
+                    readOnly = true,
+                    trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded2) },
+                    modifier = Modifier.menuAnchor()
+                )
+
+                ExposedDropdownMenu(
+                    expanded = expanded2,
+                    onDismissRequest = { expanded2 = false }
+                ) {
+                    list2.forEach { item ->
+                        DropdownMenuItem(
+                            text = { Text(text = item) },
+                            onClick = {
+                                selectedText2 = item
+                                expanded2 = false
+                                Toast.makeText(context, item, Toast.LENGTH_SHORT).show()
+                            }
+                        )
+                    }
+                }
+            }
+        }
     }
-    return selectedText
-}
-
-
-//Esto mostrará un textfield para que se escriba el string
-@Composable
-fun MostrarTextField(textoMostrar: String){
-
-        return TextField(
-            value = "",
-            onValueChange = { /*TODO*/ },
-            label = { Text(textoMostrar) },
-        )
-
 }
 
 @Preview(showBackground = true)
