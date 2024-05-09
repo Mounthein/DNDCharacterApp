8f99393ee481656dcc2b4207c22df8b0755fe16a
Jironiah <seandertal@gmail.com>
2024-05-09 15:38:50 +0200

    Clase Character modificada permitir RealmList nullable y tipo de variable modificada de val a var

--- app/src/main/java/com/example/dndcharacterapp/models/character/Character.kt
+++ app/src/main/java/com/example/dndcharacterapp/models/character/Character.kt
@@ -7,43 +7,56 @@ import org.mongodb.kbson.ObjectId
 
 class Character: RealmObject {
     @PrimaryKey
-    val _id: ObjectId = ObjectId()
-    val name: String? = null
-    val level: Int? = null
-    val inspiration: Boolean = false
-    val background: String? = null
-    val race: EmRaceCh? = null
-    val alignment: EmAlignmentCh? = null
-    val hitPoints: EmHitPointsCh? = null
-    val hit_die: RealmList<EmHitDieCh>? = null
-    val death_saves: EmDeathSavesCh? = null
-    val temporary_HitPoints: Int? = null
-    val exhaustion: Int? = null
-    val armor_Class: EmArmorClassCh? = null
-    val classes: RealmList<EmClassItemCh>? = null
-    val experience_Points: Int? = null
-    val stats: RealmList<EmStatCh>? = null
-    val skill_proficiencies: RealmList<EmSkillProficiencyCh>? = null
-    val languages: RealmList<EmLanguageCh>? = null
-    val other_proficiencies: RealmList<EmProficiencyCh>? = null
-    val equipment: RealmList<EmEquipmentCh>? = null
-    val coin_pouch: RealmList<EmCoinCh>? = null
-    val features: RealmList<EmFeatureCh>? = null
-    val traits: RealmList<EmTraitCh>? = null
-    val spell_abilities: RealmList<EmSpellAbilityCh>? = null
-    val prepared_spells: RealmList<EmSpellCh>? = null
-    val known_spells: RealmList<EmSpellCh>? = null
-    val passive_wisdom: Int? = null
-    val initiative: Int? = null
-    val speed: Int? = null
-    val proficiency_bonus: Int? = null
-    val saving_throws: RealmList<String>? = null
-    val personality_traits: String? = null
-    val ideals: String? = null
-    val bonds: String? = null
-    val flaws: String? = null
-    val character_appearance: String? = null
-    val character_backstory: String? = null
-    val allies_organizations: String? = null
-    val symbol: String? = null
+    var _id: ObjectId = ObjectId()
+    var name: String? = null
+    var level: Int? = null
+    var inspiration: Boolean = false
+    var background: String? = null
+    var race: EmRaceCh? = null
+    var alignment: EmAlignmentCh? = null
+    var hitPoints: EmHitPointsCh? = null
+    var hit_die: RealmList<EmHitDieCh>? = null
+    var death_saves: EmDeathSavesCh? = null
+    var temporary_HitPoints: Int? = null
+    var exhaustion: Int? = null
+    var armor_Class: EmArmorClassCh? = null
+    var classes: RealmList<EmClassItemCh>? = null
+    var experience_Points: Int? = null
+    var stats: RealmList<EmStatCh>? = null
+    var skill_proficiencies: RealmList<EmSkillProficiencyCh>? = null
+    var languages: RealmList<EmLanguageCh>? = null
+    var other_proficiencies: RealmList<EmProficiencyCh>? = null
+    var equipment: RealmList<EmEquipmentCh>? = null
+    var coin_pouch: RealmList<EmCoinCh>? = null
+    var features: RealmList<EmFeatureCh>? = null
+    var traits: RealmList<EmTraitCh>? = null
+    var spell_abilities: RealmList<EmSpellAbilityCh>? = null
+    var prepared_spells: RealmList<EmSpellCh>? = null
+    var known_spells: RealmList<EmSpellCh>? = null
+    var passive_wisdom: Int? = null
+    var initiative: Int? = null
+    var speed: Int? = null
+    var proficiency_bonus: Int? = null
+    var saving_throws: RealmList<String> = crearRealmList()
+    var personality_traits: String? = null
+    var ideals: String? = null
+    var bonds: String? = null
+    var flaws: String? = null
+    var character_appearance: String? = null
+    var character_backstory: String? = null
+    var allies_organizations: String? = null
+    var symbol: String? = null
+
+    companion object {
+        private fun <T> crearRealmList(): RealmList<T> {
+            try {
+                val realmListClass = Class.forName("io.realm.RealmList")
+                val constructor = realmListClass.getDeclaredConstructor()
+                constructor.isAccessible = true
+                return constructor.newInstance() as RealmList<T>
+            } catch (e: Exception) {
+                throw RuntimeException("Error al crear RealmList mediante reflexión", e)
+            }
+        }
+    }
 }
